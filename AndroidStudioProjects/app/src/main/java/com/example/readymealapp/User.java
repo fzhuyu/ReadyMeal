a472a86c6b1c3e2154a293906789be123da4e68d
Jordan <jgreene1@ufl.edu>
2020-10-26 20:42:50 -0400

    Added some comments

--- AndroidStudioProjects/app/src/main/java/com/example/readymealapp/User.java
+++ AndroidStudioProjects/app/src/main/java/com/example/readymealapp/User.java
@@ -5,7 +5,6 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-//Added new stuff below
 @Entity
 public class User {
     @PrimaryKey(autoGenerate  = true)
