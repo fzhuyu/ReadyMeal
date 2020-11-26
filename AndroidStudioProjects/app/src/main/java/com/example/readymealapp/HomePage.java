b3508c15b111b5604ba410b1778697afae61ca80
Marco Jaen <marcojaenlainez@gmail.com>
2020-11-25 20:05:58 -0500

    Added onclick functionality for the Daily Diet cardview

--- AndroidStudioProjects/app/src/main/java/com/example/readymealapp/HomePage.java
+++ AndroidStudioProjects/app/src/main/java/com/example/readymealapp/HomePage.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.room.Room;
 
 import java.util.concurrent.Executor;
@@ -17,7 +18,9 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+        CardView dietcard = (CardView) findViewById(R.id.diet_card);
 
+        //dietcard.setOnClickListener(this);
 
         //instantiating the database
         final AppDatabase Local_db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "User_db").build();
@@ -38,12 +41,34 @@ public class HomePage extends AppCompatActivity {
         });
 
 
-
-
     }
 
     public void goToUserInputData(View view) {
         Intent UserInputActivity = new Intent (this, UserInput.class);
         startActivity(UserInputActivity);
     }
+
+    public void goToDailyDiet(View view){
+        Intent DailyDietActivity = new Intent (this, DailyDiet.class);
+        startActivity(DailyDietActivity);
+    }
+
+
+/*
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()){
+            case R.id.diet_card:
+                i = new Intent(this, DailyDiet.class);
+                startActivity(i);
+                break;
+
+            default:
+                break;
+        }
+    }
+    */
+
 }
